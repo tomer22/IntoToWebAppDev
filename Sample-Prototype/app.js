88aca0f29c92776c6edde35023f2398e0c1e6095
Tomer Inbar <tomerinbar66@gmail.com>
2023-02-15 22:37:53 -0500

    Lab 5

--- Sample-Prototype/app.js
+++ Sample-Prototype/app.js
@@ -38,7 +38,7 @@ app.get( "/ads", ( req, res ) => {
 } );
 
 
-const read_item_sql = 'SELECT * FROM ads WHERE ad_id = ?'
+const read_item_sql = `SELECT * FROM ads WHERE ad_id = ?`
 // define a route for the item detail page
 app.get( "/ads/item/:id", ( req, res ) => {
     // res.sendFile( __dirname + "/views/item.html" );
@@ -57,9 +57,9 @@ app.get( "/ads/item/:id", ( req, res ) => {
 
 } );
 
-const delete_item_sql = "DELETE FROM ads WHERE id = ?";
+const delete_item_sql = "DELETE FROM ads WHERE ad_id = ?";
 app.get( "/ads/item/:id/delete", ( req, res ) => {
-    db.execute(delete_item_sql, [req.params.id], ( err, result ) => {
+    db.execute(delete_item_sql, [req.params.id], ( error, result ) => {
         if (error) {
             res.status(500).send(error); // Internal server error
         } else {
@@ -67,22 +67,42 @@ app.get( "/ads/item/:id/delete", ( req, res ) => {
         }
     })
 });
-app.post("/stuff", (req, res) => {
-    res.send("Thanks for submitting your ad, our website will process it shortly")
+
+//I didn't make the post function complete yet because my vision for the website is a bit different than what this post does (I wanted the website to take in the user's input and run some script on it and then post its own output and add to the website with its "Feedback" or return based on the input)
+//My website will format the request to the AI generator and will post an output based on what the AI engine says
+const post_item_sql = `INSERT INTO ads
+(product_name, quantity)
+VALUES
+(?, ?)`
+app.post("/ads", (req, res) => {
+
+    /*res.send("Thanks for submitting your ad, our website will process it shortly")*/
+    db.execute(post_item_sql, [req.body.name, req.body.quantity], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            //results.insertId has the primary key (id) of the newly inserted element.
+            res.redirect(`/ads/item/${results.insertId}`);
+        }
+    });
 })
 
-//did not finish values for this
-const post_sql_msg = `INSERT INTO stuff (product_name, quantity) VALUES (?, ?)`
-app.post("/sample/url", ( req, res ) => {
-     
-});
-
+//This also I will update later when we learn a way to process user input and post that change to the website (for now I am doing a sample that doensn't quite work as it should)
 const update_item_sql = `
 UPDATE ads
 SET
     item = ?, quantity = ?, product_name = ?
-WHERE id = ?
+WHERE ad_id = ?
 `
+app.post("/ads/item/:ad_id", ( req, res ) => {
+    db.execute(update_item_sql, [req.body.name, req.body.quantity, req.body.description, req.params.id], (error, results) => {
+        if (error)
+            res.status(500).send(error); //Internal Server Error
+        else {
+            res.redirect(`/ads/item/${req.params.ad_id}`);
+        }
+    });
+})
 
 // start the server
 app.listen( port, () => {
